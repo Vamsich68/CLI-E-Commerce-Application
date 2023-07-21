57b74fb2946df06c7ca5d4db7ee10c5fbbaa1157
vamsi <chokkapuvamsi123@gmail.com>
2023-07-21 23:27:29 +0530

    Implementing user functions

--- mycart.py
+++ mycart.py
@@ -1,9 +1,9 @@
 import argparse
 import user
 import admin
-from user import register_user
+from user import register_user, login_user
 
-# Sample data for categories and products (You can replace this with actual data from a database or data file)
+# Sample data for categories and products
 categories = {
     1: "Electronics",
     2: "Clothing",
@@ -41,34 +41,69 @@ def view_product_details(product_id):
 def main():
     parser = argparse.ArgumentParser(description="MyCart - CLI E-commerce App")
 
-    parser.add_argument("--view_categories", action="store_true", help="View all categories")
-    parser.add_argument("--view_products", type=int, help="View products under a category (provide category ID)")
-    parser.add_argument("--view_product_details", type=int, help="View product details (provide product ID)")
+    # parser.add_argument("--view_categories", action="store_true", help="View all categories")
+    # parser.add_argument("--view_products", type=int, help="View products under a category (provide category ID)")
+    # parser.add_argument("--view_product_details", type=int, help="View product details (provide product ID)")
+
+    # args = parser.parse_args()
+
+
+
+    # Add arguments for user-related functionalities
+    parser.add_argument("--register_user", action="store_true", help="Register a new user")
+    parser.add_argument("--login", action="store_true", help="Login with user's email")
+    parser.add_argument("--name", type=str, help="User's name")
+    parser.add_argument("--email", type=str, help="User's email")
 
     args = parser.parse_args()
-    print("")
-    email=input("enter your mail: ")
-    password= input("password: ")
-    
 
-# Sample user data (You can get this from the user through the CLI)
-    user_data = {
-        'name': email,
-        'email': password
-        }
-
-# Call the register_user function with the user_data
-    register_user(user_data)
-
-
-    if args.view_categories:
-        view_categories()
-    elif args.view_products:
-        view_products(args.view_products)
-    elif args.view_product_details:
-        view_product_details(args.view_product_details)
+    # Handle the parsed arguments accordingly
+    if args.register_user:
+    # Handle user registration using the provided name and email arguments
+        if args.name and args.email:
+            user_data = {
+                'name': args.name,
+                'email': args.email
+            }
+            register_user(user_data)
+        else:
+            print("Please provide both name and email for user registration.")
     else:
         print("Please provide a valid option.")
+    if args.login:
+        if args.email:
+            logged_in_user = login_user(args.email)
+            if logged_in_user:
+            # If the user login is successful, you can continue with other functionalities for the user.
+                pass
+        else:
+            print("Please provide the email for user login.")
+    else:
+        print("Please provide a valid option.")
+
+#     print("")
+#     email=input("enter your mail: ")
+#     password= input("password: ")
+    
+
+# # Sample user data (You can get this from the user through the CLI)
+#     user_data = {
+#         'name': email,
+#         'email': password
+#         }
+
+# # Call the register_user function with the user_data
+#     register_user(user_data)
+
+
+    # if args.view_categories:
+    #     view_categories()
+    # elif args.view_products:
+    #     view_products(args.view_products)
+    # elif args.view_product_details:
+    #     view_product_details(args.view_product_details)
+    # else:
+    #     print("Please provide a valid option.")
 
 if __name__ == "__main__":
     main()
